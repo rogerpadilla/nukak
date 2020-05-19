94635a88e8151ab8c70833f2c8f4a02813626de7
Roger Padilla <roger.padilla@limelighthealth.com>
2020-05-19 11:10:50 -0500

    refactor: remove 'text' property from Cell type

--- src/datasource/mongo/mongodbQuerier.ts
+++ src/datasource/mongo/mongodbQuerier.ts
@@ -42,7 +42,7 @@ export class MongodbQuerier extends Querier {
   }
 
   async findOne<T>(type: { new (): T }, qm: QueryOneFilter<T>) {
-    if (qm.populate) {
+    if (qm.populate && Object.keys(qm.populate).length) {
       return this.collection(type)
         .aggregate(buildAggregationPipeline(type, qm))
         .toArray()
@@ -52,7 +52,7 @@ export class MongodbQuerier extends Querier {
   }
 
   async find<T>(type: { new (): T }, qm: Query<T>) {
-    if (qm.populate) {
+    if (qm.populate && Object.keys(qm.populate).length) {
       return this.collection(type).aggregate(buildAggregationPipeline(type, qm)).toArray();
     }
 
