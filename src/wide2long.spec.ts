701fdb5ace22ba38b30803c435630d426a142f99
Roger Padilla <roger.padilla@limelighthealth.com>
2020-05-21 10:03:23 -0500

    feat: support one-header and one-pk wide2long

--- src/wide2long.spec.ts
+++ src/wide2long.spec.ts
@@ -30,15 +30,74 @@ it('should parse wide to long', () => {
     ['New Jersey', 1990, 1],
     ['New Jersey', 2000, 5],
     ['Arizona', 1960, 3],
-    ['Arizona', 1960, 9],
-    ['Arizona', 1960, 8],
-    ['Arizona', 1960, 7],
-    ['Arizona', 1960, 5],
+    ['Arizona', 1970, 9],
+    ['Arizona', 1980, 8],
+    ['Arizona', 1990, 7],
+    ['Arizona', 2000, 5],
   ];
 
   const output = parseWideToLong(input, identifiers, relations);
 
-  console.log('output', output);
+  expect(output).toEqual(expected);
+});
+
+it('should parse wide (one header row one primary column) to long', () => {
+  const inputTable: string[][] = [
+    ['Option', 'A', 'B', 'C'],
+    ['0 mo-AL/AL', '0.95', '0.96', '0.97'],
+    ['0 mo-EE/AL', '0.912', '0.913', '0.914'],
+  ];
+
+  const relations: Relation[] = [
+    { key: { column: 1, row: 0 }, value: undefined },
+    { key: { column: 2, row: 0 }, value: undefined },
+    { key: { column: 3, row: 0 }, value: undefined },
+  ];
+
+  const identifiers: Cell[] = [{ column: 0, row: 0 }];
+
+  const expected = [
+    ['0 mo-AL/AL', 'A', '0.95'],
+    ['0 mo-AL/AL', 'B', '0.96'],
+    ['0 mo-AL/AL', 'C', '0.97'],
+    ['0 mo-EE/AL', 'A', '0.912'],
+    ['0 mo-EE/AL', 'B', '0.913'],
+    ['0 mo-EE/AL', 'C', '0.914'],
+  ];
+
+  const output = parseWideToLong(inputTable, identifiers, relations);
+
+  expect(output).toEqual(expected);
+});
+
+it('should parse wide (one header row two primary columns) to long', () => {
+  const inputTable: string[][] = [
+    ['Min/Max', 'Option', 'A', 'B', 'C'],
+    ['Max', '0 mo-AL/AL', '0.95', '0.96', '0.97'],
+    ['Min', '0 mo-EE/AL', '0.912', '0.913', '0.914'],
+  ];
+
+  const relations: Relation[] = [
+    { key: { column: 2, row: 0 }, value: undefined },
+    { key: { column: 3, row: 0 }, value: undefined },
+    { key: { column: 4, row: 0 }, value: undefined },
+  ];
+
+  const identifiers: Cell[] = [
+    { column: 0, row: 0 },
+    { column: 1, row: 0 },
+  ];
+
+  const expected = [
+    ['Max', '0 mo-AL/AL', 'A', '0.95'],
+    ['Max', '0 mo-AL/AL', 'B', '0.96'],
+    ['Max', '0 mo-AL/AL', 'C', '0.97'],
+    ['Min', '0 mo-EE/AL', 'A', '0.912'],
+    ['Min', '0 mo-EE/AL', 'B', '0.913'],
+    ['Min', '0 mo-EE/AL', 'C', '0.914'],
+  ];
+
+  const output = parseWideToLong(inputTable, identifiers, relations);
 
   expect(output).toEqual(expected);
 });
--- src/wide2long.ts
+++ src/wide2long.ts
@@ -1,54 +1,22 @@
-export function parseWideToLong(input: readonly any[][], identifiers: Cell[], relations: Relation[]): any[][] {
+export function parseWideToLong(input: readonly any[][], identifiers: readonly Cell[], relations: readonly Relation[]): any[][] {
   const output: any[][] = [];
-
-  const srcN = input.length;
-  const srcM = input[0].length;
-  const newM = srcN - 1;
-  const newN = (srcN - 1) * (srcM - 1);
-
-  let srcI = 1;
-  let srcJ = 0;
   let count = 0;
 
-  for (let newI = 0; newI < newN; ++newI) {
-    output[newI] = [];
-    for (let newJ = 0; newJ < newM; ++newJ) {
-      output[newI][newJ] = input[srcI][srcJ];
-      if (count % 2 === 0) {
-        srcI--;
-        srcJ++;
-      } else {
-        srcI++;
-        srcJ--;
+  for (let i = 0; i < input.length - 1; ++i) {
+    for (let j = 0; j < input[i].length - 1; ++j) {
+      // identifiers
+      output[count] = identifiers.map((id) => input[i + 1][id.column]);
+      // header
+      output[count].push(input[0][j + identifiers.length]);
+      // values
+      for (let indexRel = i + identifiers.length; indexRel <= i + identifiers.length; ++indexRel) {
+        output[count].push(input[indexRel][j + 1]);
       }
       count++;
     }
   }
 
-  //   const input = [
-  //     ['State', 1960, 1970, 1980, 1990, 2000],
-  //     ['New York', 2, 5, 2, 5, 4],
-  //     ['New Jersey', 3, 1, 4, 1, 5],
-  //     ['Arizona', 3, 9, 8, 7, 5],
-  //   ];
-
-  //   const expected = [
-  //     ['New York', 1960, 2],
-  //     ['New York', 1970, 5],
-  //     ['New York', 1980, 2],
-  //     ['New York', 1990, 5],
-  //     ['New York', 2000, 4],
-  //     ['New Jersey', 1960, 3],
-  //     ['New Jersey', 1970, 1],
-  //     ['New Jersey', 1980, 4],
-  //     ['New Jersey', 1990, 1],
-  //     ['New Jersey', 2000, 5],
-  //     ['Arizona', 1960, 3],
-  //     ['Arizona', 1960, 9],
-  //     ['Arizona', 1960, 8],
-  //     ['Arizona', 1960, 7],
-  //     ['Arizona', 1960, 5],
-  //   ];
+  console.table(output);
 
   return output;
 }
