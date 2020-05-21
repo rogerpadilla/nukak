47adc2583c383e7c8ccdb98b3d74652ebc058185
Roger Padilla <roger.padilla@limelighthealth.com>
2020-05-21 13:05:49 -0500

    feat: support one-header and one-pk wide2long

--- src/wide2long.ts
+++ src/wide2long.ts
@@ -1,14 +1,12 @@
 export function parseWideToLong(input: readonly any[][], identifiers: readonly Cell[], relations: readonly Relation[]): any[][] {
   const output: any[][] = [];
-  let longI = 0;
 
   for (let wideI = 0; wideI < input.length - 1; ++wideI) {
     for (let wideJ = 0; wideJ < relations.length; ++wideJ) {
       const idCells = identifiers.map((id) => input[wideI + 1][id.column]);
       const headerCell = input[0][wideJ + identifiers.length];
       const valueCell = input[wideI + 1][wideJ + identifiers.length];
-      output[longI] = [...idCells, headerCell, valueCell];
-      longI++;
+      output.push([...idCells, headerCell, valueCell]);
     }
   }
 
