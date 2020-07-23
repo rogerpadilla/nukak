32300dfa60d3b6b3d58ff0cb138ba4e58be99225
Roger Padilla <roger.padilla@limelighthealth.com>
2020-07-23 17:43:03 -0500

    feat: support express

--- packages/onql-express/src/index.ts
+++ packages/onql-express/src/index.ts
@@ -1,74 +1,73 @@
-// import * as express from 'express';
-// import {Request} from 'express-serve-static-core';
+import * as express from 'express';
+import { Request } from 'express-serve-static-core';
 
-// import {Query} from '../type';
-// import {getServerRepository} from '../repository/container';
-// import {formatKebabCase} from '../util/string.util';
-// import {buildQuery} from '../util/query.util';
-// import {getEntities} from '../entity';
+import { Query } from 'onql/type';
+import { getServerRepository } from 'onql/repository';
+import { buildQuery, formatKebabCase } from 'onql/util';
+import { getEntities } from 'onql/entity';
 
-// type MiddlewareOptions = {extendQuery?: ExtendQuery};
-// type ExtendQuery = <T>(type: {new (): T}, qm: Query<T>, req: Request) => Query<T>;
+type MiddlewareOptions = { extendQuery?: ExtendQuery };
+type ExtendQuery = <T>(type: { new (): T }, qm: Query<T>, req: Request) => Query<T>;
 
-// export function entitiesMiddleware(opts?: MiddlewareOptions) {
-//   const router = express.Router();
-//   for (const type of getEntities()) {
-//     const path = formatKebabCase(type.name);
-//     const subRouter = buildCrudRouter(type as {new (): object}, opts);
-//     router.use('/' + path, subRouter);
-//   }
-//   return router;
-// }
+export function entitiesMiddleware(opts?: MiddlewareOptions) {
+  const router = express.Router();
+  for (const type of getEntities()) {
+    const path = formatKebabCase(type.name);
+    const subRouter = buildCrudRouter(type as { new (): object }, opts);
+    router.use('/' + path, subRouter);
+  }
+  return router;
+}
 
-// export function buildCrudRouter<T>(type: {new (): T}, opts?: MiddlewareOptions) {
-//   const router = express.Router();
+export function buildCrudRouter<T>(type: { new (): T }, opts?: MiddlewareOptions) {
+  const router = express.Router();
 
-//   router.post('/', async (req, res) => {
-//     const id = await getServerRepository(type).insertOne(req.body);
-//     res.json({data: id});
-//   });
+  router.post('/', async (req, res) => {
+    const id = await getServerRepository(type).insertOne(req.body);
+    res.json({ data: id });
+  });
 
-//   router.put('/:id', async (req, res) => {
-//     await getServerRepository(type).updateOneById(req.params.id, req.body);
-//     res.json({data: req.params.id});
-//   });
+  router.put('/:id', async (req, res) => {
+    await getServerRepository(type).updateOneById(req.params.id, req.body);
+    res.json({ data: req.params.id });
+  });
 
-//   router.get('/one', async (req, res) => {
-//     const qm = assembleQuery<T>(type, req, opts?.extendQuery);
-//     const model = await getServerRepository(type).findOne(qm);
-//     res.json({data: model});
-//   });
+  router.get('/one', async (req, res) => {
+    const qm = assembleQuery<T>(type, req, opts?.extendQuery);
+    const model = await getServerRepository(type).findOne(qm);
+    res.json({ data: model });
+  });
 
-//   router.get('/:id', async (req, res) => {
-//     const qm = assembleQuery<T>(type, req, opts?.extendQuery);
-//     const model = await getServerRepository(type).findOneById(req.params.id, qm);
-//     res.json({data: model});
-//   });
+  router.get('/:id', async (req, res) => {
+    const qm = assembleQuery<T>(type, req, opts?.extendQuery);
+    const model = await getServerRepository(type).findOneById(req.params.id, qm);
+    res.json({ data: model });
+  });
 
-//   router.get('/', async (req, res) => {
-//     const qm = assembleQuery<T>(type, req, opts?.extendQuery);
-//     const models = await getServerRepository(type).find(qm);
-//     res.json({data: models});
-//   });
+  router.get('/', async (req, res) => {
+    const qm = assembleQuery<T>(type, req, opts?.extendQuery);
+    const models = await getServerRepository(type).find(qm);
+    res.json({ data: models });
+  });
 
-//   router.delete('/:id', async (req, res) => {
-//     const affectedRows = await getServerRepository(type).removeOneById(req.params.id);
-//     res.json({data: affectedRows});
-//   });
+  router.delete('/:id', async (req, res) => {
+    const affectedRows = await getServerRepository(type).removeOneById(req.params.id);
+    res.json({ data: affectedRows });
+  });
 
-//   router.delete('/', async (req, res) => {
-//     const qm = assembleQuery(type, req, opts?.extendQuery);
-//     const affectedRows = await getServerRepository(type).remove(qm.filter);
-//     res.json({data: affectedRows});
-//   });
+  router.delete('/', async (req, res) => {
+    const qm = assembleQuery(type, req, opts?.extendQuery);
+    const affectedRows = await getServerRepository(type).remove(qm.filter);
+    res.json({ data: affectedRows });
+  });
 
-//   return router;
-// }
+  return router;
+}
 
-// function assembleQuery<T>(type: {new (): T}, req: Request, extendQuery?: ExtendQuery) {
-//   const qm = buildQuery<T>(req.query);
-//   if (extendQuery) {
-//     return extendQuery(type, qm, req);
-//   }
-//   return qm;
-// }
+function assembleQuery<T>(type: { new (): T }, req: Request, extendQuery?: ExtendQuery) {
+  const qm = buildQuery<T>(req.query);
+  if (extendQuery) {
+    return extendQuery(type, qm, req);
+  }
+  return qm;
+}
