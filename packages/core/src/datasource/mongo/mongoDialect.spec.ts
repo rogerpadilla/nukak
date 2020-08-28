cc8f680036eb67aa9e3041f1b15ac4acaa9796f7
Roger Padilla <roger.padilla@limelighthealth.com>
2020-08-28 15:40:03 -0500

    test: support for mongodb

--- packages/core/src/datasource/mongo/mongoDialect.spec.ts
+++ packages/core/src/datasource/mongo/mongoDialect.spec.ts
@@ -1,4 +1,5 @@
-import { Item } from '../../entity/entityMock';
+import { ObjectId } from 'mongodb';
+import { Item, TaxCategory } from '../../entity/entityMock';
 import { MongoDialect } from './mongoDialect';
 
 let dialect: MongoDialect;
@@ -19,6 +20,21 @@ it('buildFilter', () => {
   expect(
     dialect.buildFilter<Item>(Item, { $and: { code: '123', name: 'abc' } })
   ).toEqual({ $and: [{ code: '123' }, { name: 'abc' }] });
+  expect(
+    dialect.buildFilter<Item>(Item, { id: '507f191e810c19729de860ea' })
+  ).toEqual({ _id: new ObjectId('507f191e810c19729de860ea') });
+  expect(
+    dialect.buildFilter<Item>(Item, { id: new ObjectId('507f191e810c19729de860ea') as any })
+  ).toEqual({ _id: new ObjectId('507f191e810c19729de860ea') });
+  expect(
+    dialect.buildFilter<TaxCategory>(TaxCategory, { id: '507f191e810c19729de860ea' })
+  ).toEqual({ id: '507f191e810c19729de860ea' });
+  expect(
+    dialect.buildFilter<TaxCategory>(TaxCategory, { pk: '507f191e810c19729de860ea' })
+  ).toEqual({ _id: new ObjectId('507f191e810c19729de860ea') });
+  expect(
+    dialect.buildFilter<TaxCategory>(TaxCategory, { pk: new ObjectId('507f191e810c19729de860ea') as any })
+  ).toEqual({ _id: new ObjectId('507f191e810c19729de860ea') });
 });
 
 it('buildAggregationPipeline empty', () => {
