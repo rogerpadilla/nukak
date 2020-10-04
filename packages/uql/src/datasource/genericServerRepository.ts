6096eccb3dc9824e5ce95c86882a426d16f1d5e9
Roger Padilla <roger.padilla@limelighthealth.com>
2020-10-04 13:52:22 -0500

    test: improve tests

--- packages/uql/src/datasource/genericServerRepository.ts
+++ packages/uql/src/datasource/genericServerRepository.ts
@@ -10,17 +10,13 @@ export class GenericServerRepository<T, ID = any> implements ServerRepository<T,
   }
 
   @Transactional({ propagation: 'required' })
-  async insertOne(body: T, @InjectQuerier() querier?: Querier<ID>) {
-    const id = await querier.insertOne(this.meta.type, body);
-    await this.insertRelations({ ...body, [this.meta.id.property]: id }, querier);
-    return id;
+  insertOne(body: T, @InjectQuerier() querier?: Querier<ID>) {
+    return querier.insertOne(this.meta.type, body);
   }
 
   @Transactional({ propagation: 'required' })
-  async updateOneById(id: ID, body: T, @InjectQuerier() querier?: Querier<ID>) {
-    const affectedRows = await querier.update(this.meta.type, { [this.meta.id.property]: id }, body);
-    await this.updateRelations({ ...body, [this.meta.id.property]: id }, querier);
-    return affectedRows;
+  updateOneById(id: ID, body: T, @InjectQuerier() querier?: Querier<ID>) {
+    return querier.updateOneById(this.meta.type, id, body);
   }
 
   @Transactional({ propagation: 'required' })
@@ -61,68 +57,5 @@ export class GenericServerRepository<T, ID = any> implements ServerRepository<T,
   @Transactional()
   count(filter: QueryFilter<T>, @InjectQuerier() querier?: Querier<ID>) {
     return querier.count(this.meta.type, filter);
-  }
-
-  protected async insertRelations(body: T, querier: Querier<ID>) {
-    const id = body[this.meta.id.property];
-
-    const insertProms = this.filterIndependentRelations(body).map((prop) => {
-      const relOpts = this.meta.relations[prop];
-      const relType = relOpts.type();
-      if (relOpts.cardinality === 'oneToOne') {
-        const relBody: T = body[prop];
-        return querier.insertOne(relType, relBody);
-      }
-      if (relOpts.cardinality === 'oneToMany') {
-        const relBody: T[] = body[prop];
-        relBody.forEach((it: T) => {
-          it[relOpts.mappedBy] = id;
-        });
-        return querier.insert(relType, relBody);
-      }
-      if (relOpts.cardinality === 'manyToMany') {
-        throw new TypeError(`unsupported update cardinality ${relOpts.cardinality}`);
-      }
-    });
-
-    await Promise.all<any>(insertProms);
-  }
-
-  protected async updateRelations(body: T, querier: Querier<ID>) {
-    const id = body[this.meta.id.property];
-
-    const removeProms = this.filterIndependentRelations(body).map(async (prop) => {
-      const relOpts = this.meta.relations[prop];
-      const relType = relOpts.type();
-      if (relOpts.cardinality === 'oneToOne') {
-        const relBody: T = body[prop];
-        if (relBody === null) {
-          return querier.remove(relType, { [relOpts.mappedBy]: id });
-        }
-        return querier.update(relType, { [relOpts.mappedBy]: id }, relBody);
-      }
-      if (relOpts.cardinality === 'oneToMany') {
-        const relBody: T[] = body[prop];
-        await querier.remove(relType, { [relOpts.mappedBy]: id });
-        if (relBody !== null) {
-          relBody.forEach((it: T) => {
-            it[relOpts.mappedBy] = id;
-          });
-          return querier.insert(relType, relBody);
-        }
-      }
-      if (relOpts.cardinality === 'manyToMany') {
-        throw new TypeError(`unsupported update cardinality ${relOpts.cardinality}`);
-      }
-    });
-
-    await Promise.all(removeProms);
-  }
-
-  protected filterIndependentRelations(body: T) {
-    return Object.keys(body).filter((prop) => {
-      const relOpts = this.meta.relations[prop];
-      return relOpts && relOpts.cardinality !== 'manyToOne';
-    });
   }
 }
--- packages/uql/src/type/querier.ts
+++ packages/uql/src/type/querier.ts
@@ -1,4 +1,5 @@
 import { getEntityMeta } from 'uql/decorator';
+import { EntityMeta } from './entity';
 import { Query, QueryFilter, QueryOne, QueryOptions } from './query';
 
 /**
@@ -8,26 +9,30 @@ export abstract class Querier<ID = any> {
   abstract insert<T>(type: { new (): T }, body: T[], opts?: QueryOptions): Promise<ID[]>;
 
   async insertOne<T>(type: { new (): T }, body: T, opts?: QueryOptions) {
-    const ids = await this.insert(type, [body], opts);
-    return ids[0];
+    const [id] = await this.insert(type, [body], opts);
+    const meta = getEntityMeta(type);
+    await this.insertRelations(type, { ...body, [meta.id.property]: id });
+    return id;
   }
 
   abstract update<T>(type: { new (): T }, filter: QueryFilter<T>, body: T): Promise<number>;
 
-  updateOneById<T>(type: { new (): T }, id: ID, body: T) {
+  async updateOneById<T>(type: { new (): T }, id: ID, body: T) {
     const meta = getEntityMeta(type);
-    return this.update(type, { [meta.id.property]: id }, body);
+    const affectedRows = await this.update(type, { [meta.id.property]: id }, body);
+    await this.updateRelations(type, { ...body, [meta.id.property]: id });
+    return affectedRows;
   }
 
   abstract find<T>(type: { new (): T }, qm: Query<T>, opts?: QueryOptions): Promise<T[]>;
 
   async findOne<T>(type: { new (): T }, qm: Query<T>, opts?: QueryOptions) {
     qm.limit = 1;
-    const rows = await this.find(type, qm, opts);
-    return rows ? rows[0] : undefined;
+    const [row] = await this.find(type, qm, opts);
+    return row;
   }
 
-  async findOneById<T>(type: { new (): T }, id: ID, qo: QueryOne<T>, opts?: QueryOptions) {
+  findOneById<T>(type: { new (): T }, id: ID, qo: QueryOne<T>, opts?: QueryOptions) {
     const meta = getEntityMeta(type);
     return this.findOne(type, { ...qo, filter: { [meta.id.property]: id } }, opts);
   }
@@ -52,6 +57,73 @@ export abstract class Querier<ID = any> {
   abstract rollbackTransaction(): Promise<void>;
 
   abstract release(): Promise<void>;
+
+  protected async insertRelations<T>(type: { new (): T }, body: T) {
+    const meta = getEntityMeta(type);
+
+    const id = body[meta.id.property];
+
+    const insertProms = this.filterIndependentRelations(meta, body).map((prop) => {
+      const relOpts = meta.relations[prop];
+      const relType = relOpts.type();
+      if (relOpts.cardinality === 'oneToOne') {
+        const relBody: T = body[prop];
+        return this.insertOne(relType, relBody);
+      }
+      if (relOpts.cardinality === 'oneToMany') {
+        const relBody: T[] = body[prop];
+        relBody.forEach((it: T) => {
+          it[relOpts.mappedBy] = id;
+        });
+        return this.insert(relType, relBody);
+      }
+      if (relOpts.cardinality === 'manyToMany') {
+        throw new TypeError(`unsupported update cardinality ${relOpts.cardinality}`);
+      }
+    });
+
+    await Promise.all<any>(insertProms);
+  }
+
+  protected async updateRelations<T>(type: { new (): T }, body: T) {
+    const meta = getEntityMeta(type);
+
+    const id = body[meta.id.property];
+
+    const removeProms = this.filterIndependentRelations(meta, body).map(async (prop) => {
+      const relOpts = meta.relations[prop];
+      const relType = relOpts.type();
+      if (relOpts.cardinality === 'oneToOne') {
+        const relBody: T = body[prop];
+        if (relBody === null) {
+          return this.remove(relType, { [relOpts.mappedBy]: id });
+        }
+        return this.update(relType, { [relOpts.mappedBy]: id }, relBody);
+      }
+      if (relOpts.cardinality === 'oneToMany') {
+        const relBody: T[] = body[prop];
+        await this.remove(relType, { [relOpts.mappedBy]: id });
+        if (relBody !== null) {
+          relBody.forEach((it: T) => {
+            it[relOpts.mappedBy] = id;
+          });
+          return this.insert(relType, relBody);
+        }
+      }
+      if (relOpts.cardinality === 'manyToMany') {
+        throw new TypeError(`unsupported update cardinality ${relOpts.cardinality}`);
+      }
+    });
+
+    await Promise.all(removeProms);
+  }
+
+  protected filterIndependentRelations<T>(meta: EntityMeta<T>, body: T) {
+    return Object.keys(body).filter((prop) => {
+      const relOpts = meta.relations[prop];
+      return relOpts && relOpts.cardinality !== 'manyToOne';
+    });
+  }
 }
 
 export interface QuerierPoolOptions {
