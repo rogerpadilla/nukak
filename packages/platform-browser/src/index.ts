bd6fb97e05c432f99ecf17d915336f78d51b6a7d
Roger Padilla <rogerjose81@gmail.com>
2020-11-25 16:27:34 -0500

    refactor: move to @uql organization packaging

--- packages/platform-browser/src/index.ts
+++ packages/platform-browser/src/index.ts
@@ -1,4 +1,4 @@
-export * from '@uql/core/type/config';
+export * from '@uql/core/type/options';
 export * from '@uql/core/type/entity';
 export * from '@uql/core/type/query';
 export * from '@uql/core/options';
