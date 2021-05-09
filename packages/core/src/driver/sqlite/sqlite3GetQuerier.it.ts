43a7acf60cf78842b8b4471ea5ea7754f1feeaf8
Roger <rogerjose81@gmail.com>
2021-05-09 16:10:54 -0500

    refactor: improve queries by filtering properties and relations by type

---
+++ packages/core/src/driver/sqlite/sqlite3GetQuerier.it.ts
@@ -0,0 +1,24 @@
+import { clearOptions, setOptions } from '../../options';
+import { getQuerier } from '../../querier/querierPool';
+
+describe('querierPool', () => {
+  beforeEach(() => {
+    setOptions({
+      datasource: {
+        driver: 'sqlite3',
+        filename: ':memory:',
+      },
+    });
+  });
+
+  afterEach(() => {
+    clearOptions();
+  });
+
+  it('getQuerier', async () => {
+    const querier1 = await getQuerier();
+    expect(querier1).toBeDefined();
+    const querier2 = await getQuerier();
+    expect(querier2).toBeDefined();
+  });
+});
