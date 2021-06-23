ad09bbf6168d034442bb41808e34db89a3e4c671
Roger <rogerjose81@gmail.com>
2021-06-23 17:08:46 -0500

    feat: support relations in  (#21)

--- packages/client/src/querier/querier.util.spec.ts
+++ packages/client/src/querier/querier.util.spec.ts
@@ -22,7 +22,7 @@ it('stringifyQuery', () => {
   expect(stringifyQuery({})).toBe('');
   expect(stringifyQuery({ project: undefined })).toBe('?project=undefined');
   const source: Query<Item> = {
-    $project: { id: 1, name: 1, tax: {}, measureUnit: { $project: { id: 1, name: 1, categoryId: 1 } } },
+    $project: { id: 1, name: 1, tax: true, measureUnit: { $project: { id: 1, name: 1, categoryId: 1 } } },
     $filter: { name: 'Batman', companyId: 38 },
     $group: ['companyId'],
     $sort: { companyId: 1, name: -1 },
@@ -30,6 +30,6 @@ it('stringifyQuery', () => {
   };
   const result = stringifyQuery(source);
   const expected =
-    '?$project={"id":1,"name":1,"tax":{},"measureUnit":{"$project":{"id":1,"name":1,"categoryId":1}}}&$filter={"name":"Batman","companyId":38}&$group=["companyId"]&$sort={"companyId":1,"name":-1}&$limit=5';
+    '?$project={"id":1,"name":1,"tax":true,"measureUnit":{"$project":{"id":1,"name":1,"categoryId":1}}}&$filter={"name":"Batman","companyId":38}&$group=["companyId"]&$sort={"companyId":1,"name":-1}&$limit=5';
   expect(result).toBe(expected);
 });
