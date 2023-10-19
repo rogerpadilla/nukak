35c214fead22e35c8bac25b30b31dacc1b7c7d5c
Roger <rogerjose81@gmail.com>
2023-10-19 10:42:39 -0500

    test: improve test coverage by simplifying code

--- packages/nukak-mysql/src/mysqlDialect.ts
+++ packages/nukak-mysql/src/mysqlDialect.ts
@@ -1,13 +1,9 @@
 import sqlstring from 'sqlstring';
 import { AbstractSqlDialect } from 'nukak/dialect';
-import { QueryRaw, Scalar } from 'nukak/type';
-import { getRawValue } from 'nukak/util';
+import { Scalar } from 'nukak/type';
 
 export class MySqlDialect extends AbstractSqlDialect {
   override escape(value: any): Scalar {
-    if (value instanceof QueryRaw) {
-      return getRawValue({ value, dialect: this });
-    }
     return sqlstring.escape(value);
   }
 }
